9ac3b33f6cd242cc4b33ca4678b584202330f6d1
cebanauskes <59064380+cebanauskes@users.noreply.github.com>
2020-09-24 22:56:38 +0300

    second_deploy1

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -24,7 +24,6 @@ from django.contrib.flatpages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', include('django.contrib.flatpages.urls')),
     path('', include('recipes.urls')),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
